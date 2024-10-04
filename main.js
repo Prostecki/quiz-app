4ff37f172e2fdca7c2e7d069fe8467183bf5e5be
Mark Taratynov <mark.taratynov@gmail.com>
2024-10-04 14:37:04 +0200

    Two api requests at the same time

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
+//Start the quiz
 const startQuiz = document.querySelector(".start-button");
+
+const videoGamesQuestions =
+  "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple";
+const sportQuestions =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple";
 console.log(startQuiz);
 startQuiz.addEventListener("click", getDataQuestions);
 async function getDataQuestions() {
   try {
-    const response = await fetch(
-      "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple"
-    );
-    const questions = await response.json();
-    renderQuestions(questions.results);
+    const videoCategory = await fetch(videoGamesQuestions);
+    const sportCategory = await fetch(sportQuestions);
+    //using promise all to wait for api requests to resolve
+    const [response1, response2] = await Promise.all([
+      videoCategory,
+      sportCategory,
+    ]);
+
+    const videoData = await response1.json();
+    const sportData = await response2.json();
+    console.log(videoData, sportData);
   } catch {
-    console.error("Something wrong!");
+    console.error("Error fetching data from APIs:", error);
   }
 }
 
