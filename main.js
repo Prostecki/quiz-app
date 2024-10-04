2a49491a22962c6f7beff5b54b0eb1eb219d6188
Mark Taratynov <mark.taratynov@gmail.com>
2024-10-04 14:47:41 +0200

    Implement one category

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //Start the quiz
 const startQuiz = document.querySelector(".start-button");
 
+const welcomeSection = document.querySelector("section");
+console.log(welcomeSection);
+
 const videoGamesQuestions =
   "https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple";
 const sportQuestions =
@@ -10,23 +13,29 @@ startQuiz.addEventListener("click", getDataQuestions);
 async function getDataQuestions() {
   try {
     const videoCategory = await fetch(videoGamesQuestions);
-    const sportCategory = await fetch(sportQuestions);
+    // const sportCategory = await fetch(sportQuestions);
     //using promise all to wait for api requests to resolve
-    const [response1, response2] = await Promise.all([
-      videoCategory,
-      sportCategory,
-    ]);
+    // const [response1, response2] = await Promise.all([
+    //   videoCategory,
+    //   sportCategory,
+    // ]);
 
-    const videoData = await response1.json();
-    const sportData = await response2.json();
-    console.log(videoData, sportData);
-  } catch {
+    const videoData = await videoCategory.json();
+    // const sportData = await response2.json();
+    console.log(videoData);
+    renderQuestions(videoData);
+  } catch (error) {
     console.error("Error fetching data from APIs:", error);
   }
 }
 
-function renderQuestions(questions) {
-  questions.forEach((element) => {
+function renderQuestions(videoCategory) {
+  welcomeSection.remove();
+  let multipleContainer = document.createElement("div");
+  multipleContainer.textContent = "Multiple Questions!";
+  document.body.appendChild(multipleContainer);
+
+  videoCategory.results.forEach((element) => {
     console.log(element);
   });
 }
